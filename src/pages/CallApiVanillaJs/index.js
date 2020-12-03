12e2ad32d81c0c3a65deeeb14b3eb6ccbe742a72
Kyohei7 <rizkimuhammad2301@gmail.com>
2020-12-03 15:00:49 +0700

    Learn Consume & Show Data From Api with Vanilla Javascript

--- src/pages/CallApiVanillaJs/index.js
+++ src/pages/CallApiVanillaJs/index.js
@@ -1,22 +1,66 @@
-import React, { useEffect } from 'react' 
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useEffect, useState } from 'react' 
+import { Button, Image, StyleSheet, Text, View } from 'react-native'
 
 const CallApiVanillaJs = () => {
 
+    // State Get Data Api
+    const [dataUser, setDataUser] = useState({
+        avatar: '',
+        email: '',
+        first_name: '',
+        last_name: '',
+    })
+
+    // State Post Data Api
+    const [dataJob, setDataJob] = useState({
+        name: '',
+        job: '',
+    })
+
+
     useEffect(() => {
         // Call Api method GET
-        fetch('https://reqres.in/api/users?page=2')
-            .then(response => response.json())
-            .then(json => console.log(json))
+        // fetch('https://reqres.in/api/users?page=2')
+        //     .then(response => response.json())
+        //     .then(json => console.log(json))
 
         // Call Api method POST
+        // const dataApi = {
+        //     name: "Muhammad Rizki",
+        //     job: "Mobile Developer"
+        // }
+
+        // console.log('Data Object: ', dataApi);
+        // console.log('Data Stringify: ', JSON.stringify(dataApi));
+
+        // fetch('https://reqres.in/api/users', {
+        //     method: 'POST',
+        //     headers: {
+        //         'Content-Type': 'application/json'
+        //     },
+        //     body: JSON.stringify(dataApi)
+        // })
+        //     .then(response => response.json())
+        //     .then(json => console.log('Post Response: ', json))
+    }, [])
+
+
+    // Get Data Api
+    const getDataApi = () => {
+        fetch('https://reqres.in/api/users/2')
+            .then(response => response.json())
+            .then(json => {
+                console.log(json)
+                setDataUser(json.data)
+            })
+    }
+
+    // Post Data Api
+    const postDataApi = () => {
         const dataApi = {
             name: "Muhammad Rizki",
-            job: "Mobile Developer"
+            job: "React Native Developer"
         }
-
-        console.log('Data Object: ', dataApi);
-        console.log('Data Stringify: ', JSON.stringify(dataApi));
 
         fetch('https://reqres.in/api/users', {
             method: 'POST',
@@ -26,12 +70,27 @@ const CallApiVanillaJs = () => {
             body: JSON.stringify(dataApi)
         })
             .then(response => response.json())
-            .then(json => console.log('Post Response: ', json))
-    }, [])
+            .then(json => {
+                console.log('Post Response: ', json)
+                setDataJob(json)
+            })
+    }
+
+
 
     return(
         <View style={styles.wrapper} >
             <Text style={styles.textTittle} >Call Api with Vanilla Javascript</Text>
+            <Button title="GET DATA API" onPress={getDataApi} />
+            <Text> Response Get Data Api</Text>
+            <Image source={{uri: dataUser.avatar}} style={styles.avatar} />
+            <Text> {`${dataUser.first_name} ${dataUser.last_name}`} </Text>
+            <Text> {dataUser.email} </Text>
+            <View style={styles.line} />
+            <Button title="Post DATA API" onPress={postDataApi} />
+            <Text> Response Post Data Api</Text>
+            <Text> {dataJob.name} </Text>
+            <Text> {dataJob.job} </Text>
         </View>
     )
 }
@@ -48,4 +107,15 @@ const styles = StyleSheet.create({
         marginTop: 30,
         fontSize: 20
     },
+    line: {
+        height: 2,
+        backgroundColor: 'grey',
+        marginVertical: 20
+    },
+    avatar: {
+        width: 100, 
+        height: 100, 
+        borderRadius: 50
+    }
+    
 })
